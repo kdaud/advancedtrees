39cad9c40ffde0bd4f2ce56581a4bcccc8a2562b
kdaud <dkakumirizii@gmail.com>
2020-01-28 03:13:50 +0300

    files

--- src/com/opensource/Project.java
+++ src/com/opensource/Project.java
@@ -11,5 +11,62 @@ public class Project {
 	
 	private String description;
 	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
 	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the projectitle
+	 */
+	public String getProjectitle() {
+		return projectitle;
+	}
+	
+	/**
+	 * @param projectitle the projectitle to set
+	 */
+	public void setProjectitle(String projectitle) {
+		this.projectitle = projectitle;
+	}
+	
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
 }
