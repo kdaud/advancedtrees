2a943d9aeb433ddc2e33d9e03d3480652a00be4d
kdaud <dkakumirizii@gmail.com>
2020-01-28 03:57:54 +0300

    files

--- src/com/annotation/Framework.java
+++ src/com/annotation/Framework.java
@@ -3,6 +3,7 @@ package com.annotation;
 
 public class Framework extends MobilePhone {
 	
+
 	@Override
 	public void code() {
 		System.out.println("Am in my way to over there in the glory land");
--- src/com/annotation/Main.java
+++ src/com/annotation/Main.java
@@ -3,4 +3,9 @@ package com.annotation;
 
 public class Main {
 	
+	public static void main(String[] args) {
+		Framework obj = new Framework();
+		obj.code();
+	}
+	
 }
