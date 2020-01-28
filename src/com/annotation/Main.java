9bcfbf1d8086ea81b77ff4329053f5a8a9a27aa5
kdaud <dkakumirizii@gmail.com>
2020-01-28 04:26:35 +0300

    files

--- src/com/annotation/Main.java
+++ src/com/annotation/Main.java
@@ -8,6 +8,13 @@ public class Main {
 		obj.code();
 		//obj.notify();
 		obj.mycodes();
+		System.out.println(Math.abs(-567.8));
+		System.out.println(Math.ceil(27.12));
+		System.out.println(Math.floor(39.999));
+		System.out.println(Math.max(29, 13));
+		System.out.println(Math.min(34, 27));
+		System.out.println(Math.sqrt(47));
+		System.out.println(Math.pow(7, 2));
 	}
 	
 }
---
+++ src/com/annotation/Project.java
@@ -0,0 +1,9 @@
+package com.annotation;
+
+@FunctionalInterface
+public interface Project {
+	
+	void myproject();
+	//void show();
+	
+}
