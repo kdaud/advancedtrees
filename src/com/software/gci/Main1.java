382740defc2c54cc503dd60facc0d353149bbb98
kdaud <dkakumirizii@gmail.com>
2020-02-10 14:43:05 +0300

    code

--- src/com/software/gci/Main1.java
+++ src/com/software/gci/Main1.java
@@ -4,6 +4,21 @@ public class Main1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World in Africa");
+	new TestingCodes() {
+		
+		@Override
+		public void opensource() {
+			String univ="London ";
+				StringBuffer stringBuffer = new StringBuffer(univ);
+				stringBuffer.append(" University!!!");
+				System.out.println(stringBuffer);
+				
+				String name = new String("Java");
+				name.charAt(3);
+				System.out.println(name);
+		}
+		}.opensource();
+		System.out.println("Am in my way to programming great things!!!");
 	}
 	
 }
