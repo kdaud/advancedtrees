6c6032e652c015ee401e7e0b433aeaad0fdfaa26
kdaud <dkakumirizii@gmail.com>
2020-02-11 11:05:03 +0300

    files

--- src/com/collections/Collect.java
+++ src/com/collections/Collect.java
@@ -6,11 +6,9 @@ import java.util.Collection;
 public class Collect {
 	
 	public void collect() {
-		Collection student = new ArrayList();
-		student.add(34);
-		student.add(68.9);
-		student.add(34.9f);
-		student.add('d');
+		Collection<String> student = new ArrayList<>();
+		
+		student.add("Daud");
 	}
 	
 }
