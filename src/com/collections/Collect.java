4f840a08c21cbe05ee17970425c769daa9bb49df
kdaud <dkakumirizii@gmail.com>
2020-02-11 11:03:00 +0300

    codes' ; exit

---
+++ src/com/collections/Collect.java
@@ -0,0 +1,16 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Collect {
+	
+	public void collect() {
+		Collection student = new ArrayList();
+		student.add(34);
+		student.add(68.9);
+		student.add(34.9f);
+		student.add('d');
+	}
+	
+}
