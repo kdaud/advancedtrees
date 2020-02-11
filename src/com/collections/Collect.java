3aa1c05d25c711c80b8fa7aa5d4d62639542b7e4
kdaud <dkakumirizii@gmail.com>
2020-02-11 12:09:27 +0300

    files

--- src/com/collections/Collect.java
+++ src/com/collections/Collect.java
@@ -2,8 +2,8 @@ package com.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Collect {
 	
@@ -11,7 +11,7 @@ public class Collect {
 		Collection<String> student = new ArrayList<>();
 		
 		student.add("Daud");
-		Set<Integer> marks = new TreeSet<>();
+		Set<Integer> marks = new HashSet<>();
 		marks.add(2);
 		marks.add(9);
 		marks.add(5);
@@ -19,6 +19,7 @@ public class Collect {
 		marks.add(1);
 		marks.add(7);
 		marks.add(3);
+		marks.add(1);
 		for (int jj : marks) {
 			System.out.println(jj);
 		}
