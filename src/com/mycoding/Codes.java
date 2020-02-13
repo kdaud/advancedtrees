0f13cf65206b0a655f0526eee9874227ce8f828a
kdaud <dkakumirizii@gmail.com>
2020-02-13 11:13:37 +0300

    codes

---
+++ src/com/mycoding/Codes.java
@@ -0,0 +1,14 @@
+package com.mycoding;
+
+
+public class Codes {
+	
+	public static void main(String[] args) {
+		Integer [] points= {15,17,10,9,18,6};
+		Generic.printArray(points);
+		
+		
+		
+	}
+	
+}
