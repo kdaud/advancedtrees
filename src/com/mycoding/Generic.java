2a008ed15bc9d5397ee88937eed55ed0e469e84e
kdaud <dkakumirizii@gmail.com>
2020-02-13 11:09:12 +0300

    codes

--- src/com/mycoding/Generic.java
+++ src/com/mycoding/Generic.java
@@ -3,4 +3,10 @@ package com.mycoding;
 
 public class Generic {
 	
+	public static void printArray(Integer[] array) {
+		for (Integer element : array) {
+			System.out.println(element);
+		}
+	}
+	
 }
