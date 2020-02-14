80b7dba4d722e5aed11aee0116a44b14ef98863b
kdaud <dkakumirizii@gmail.com>
2020-02-14 11:00:10 +0300

    codes

---
+++ src/com/serializable/save/DOA.java
@@ -0,0 +1,14 @@
+package com.serializable.save;
+
+
+public class DOA {
+	
+	public void dosave() {
+		Student obj = new Student();
+		obj.setStudentId(10);
+		obj.setName("Daud Kakumirizi");
+		obj.setCourse("Software Engineering");
+		
+	}
+	
+}
---
+++ src/com/serializable/save/Student.java
@@ -0,0 +1,55 @@
+package com.serializable.save;
+
+
+public class Student {
+	
+	private int studentId;
+	
+	private String name;
+	
+	private String course;
+	
+	/**
+	 * @return the studentId
+	 */
+	public int getStudentId() {
+		return studentId;
+	}
+	
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the course
+	 */
+	public String getCourse() {
+		return course;
+	}
+	
+	/**
+	 * @param course the course to set
+	 */
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	
+
+}
