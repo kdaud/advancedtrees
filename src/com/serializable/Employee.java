d4d557fc52701e726f05bbcb2676e2cbcaf0796e
kdaud <dkakumirizii@gmail.com>
2020-02-14 10:29:26 +0300

    codes

--- src/com/serializable/Employee.java
+++ src/com/serializable/Employee.java
@@ -6,17 +6,21 @@ public class Employee implements Serializable {
 	
 	private int id;
 	
+	transient private String univ;
+	
 	private String name;
 	
 	/**
 	 * @param id
 	 * @param name
 	 */
-	public Employee(int id, String name) {
+	public Employee(int id, String univ, String name) {
 		super();
 		this.id = id;
+		this.univ = univ;
 		this.name = name;
-		System.out.println("ID: " + id + " " + "Name: " + name);
+		
+		System.out.println(" ID: " + id + " " + "\nUniversity: " + univ + "\nName: " + name);
 	}
 	
 }
--- src/com/serializable/Launcher.java
+++ src/com/serializable/Launcher.java
@@ -4,7 +4,7 @@ package com.serializable;
 public class Launcher {
 	
 	public static void main(String[] args) {
-		new Employee(108, "Katende Stephen");
+		new Employee(108, "Bugema University", "Katende Stephen");
 	}
 	
 }
