a4e61734dcfa924740aaa5f42de69966590b442f
kdaud <dkakumirizii@gmail.com>
2020-02-14 11:23:05 +0300

    codes

--- src/com/serializable/save/DOA.java
+++ src/com/serializable/save/DOA.java
@@ -1,13 +1,15 @@
 package com.serializable.save;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class DOA {
 	
-	public static void dosave() throws IOException {
+	public static void dosave() throws IOException, Exception {
 		Student obj = new Student();
 		obj.setStudentId(10);
 		obj.setName("Daud Kakumirizi");
@@ -18,6 +20,12 @@ public class DOA {
 		FileOutputStream outputStream= new FileOutputStream(file);
 		ObjectOutputStream objectOutputStream=new ObjectOutputStream(outputStream);
 		objectOutputStream.writeObject(obj);
+		
+		//retrieving the data from the file
+		FileInputStream fileInputStream= new FileInputStream(file);
+		ObjectInputStream objectInputStream= new ObjectInputStream(fileInputStream);
+		Student objs = (Student) objectInputStream.readObject();
+		System.out.println(objs.getName() + "\n " + objs.getCourse() + "\n" + objs.getStudentId());
 	}
 	
 }
