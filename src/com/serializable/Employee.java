2dc815f0e6272c2835715d7f61c46913faf8653f
kdaud <dkakumirizii@gmail.com>
2020-02-14 10:19:53 +0300

    codes

--- src/com/serializable/Employee.java
+++ src/com/serializable/Employee.java
@@ -7,4 +7,16 @@ public class Employee implements Serializable {
 	private int id;
 	
 	private String name;
+	
+	/**
+	 * @param id
+	 * @param name
+	 */
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+		System.out.println("ID: " + id + " " + "Name: " + name);
+	}
+	
 }
