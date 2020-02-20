7b210966c1898c93e450a5a0a1508d25ff55b2db
kdaud <dkakumirizii@gmail.com>
2020-02-20 21:26:30 +0300

    codes

---
+++ src/com/casestatement/Openmrs.java
@@ -0,0 +1,19 @@
+package com.casestatement;
+
+import java.util.Scanner;
+
+
+public class Openmrs {
+	
+	private Scanner scanner = new Scanner(System.in);
+	private static String mycourse() {
+		System.out.println("What is your district?");
+		String district="";
+		switch (district) {
+			//case "Nakasongola" -> district "Central";
+		}
+				
+	
+	}
+	
+}
